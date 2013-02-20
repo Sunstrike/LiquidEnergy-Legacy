1159da26a4b08384aa563dd2d857b3a80c87cae8
Sunstrike <sunstrike@azurenode.net>
2013-02-20 11:25:51 +0000

    Update headers and finish MJ Liquifier
    Signed-off-by: Sunstrike <sunstrike@azurenode.net>

--- common/io/sunstrike/mods/liquidenergy/blocks/tiles/TileLiquifierMJ.java
+++ common/io/sunstrike/mods/liquidenergy/blocks/tiles/TileLiquifierMJ.java
@@ -1,7 +1,11 @@
 package io.sunstrike.mods.liquidenergy.blocks.tiles;
 
+import buildcraft.api.power.IPowerProvider;
+import buildcraft.api.power.IPowerReceptor;
+import buildcraft.api.power.PowerFramework;
 import io.sunstrike.mods.liquidenergy.LiquidEnergy;
 import io.sunstrike.mods.liquidenergy.configuration.ModObjects;
+import io.sunstrike.mods.liquidenergy.configuration.Settings;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
@@ -13,22 +17,47 @@ import net.minecraftforge.liquids.LiquidTank;
 
 /**
  * TileLiquifierEU
+ * io.sunstrike.mods.liquidenergy.blocks.tiles
+ * LiquidEnergy
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Sunstrike <sunstrike@azurenode.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @author Sunstrike <sunstrike@azurenode.net>
  */
 
-public class TileLiquifierMJ extends TileEntity implements ITankContainer {
+public class TileLiquifierMJ extends TileEntity implements ITankContainer, IPowerReceptor {
 
 	// Internal buffers
 	private final LiquidTank outputBuffer = new LiquidTank(8000);
+    private IPowerProvider powerProvider = PowerFramework.currentFramework.createPowerProvider();
 
 	public TileLiquifierMJ() {
 		//LiquidEnergy.logger.info("Created TE Liquifier EU.");
 		outputBuffer.setTankPressure(100);
+        powerProvider.configure(0, 0, Settings.mjPerNv * 10, Settings.mjPerNv, Settings.mjPerNv * 100);
 	}
 
 	@Override
 	public void updateEntity() {
+        // Do work (BC)
+        if (powerProvider.getEnergyStored() >= Settings.mjPerNv) {
+            doWork();
+        }
+
 		// Dump to pipes/tanks
 		for (ForgeDirection d : new ForgeDirection[]{ForgeDirection.DOWN, ForgeDirection.UP}) {
 			int y;
@@ -48,6 +77,40 @@ public class TileLiquifierMJ extends TileEntity implements ITankContainer {
 			}
 		}
 	}
+
+    @Override
+    public void setPowerProvider(IPowerProvider provider) {
+        powerProvider = provider;
+    }
+
+    @Override
+    public IPowerProvider getPowerProvider() {
+        return powerProvider;
+    }
+
+    @Override
+    public void doWork() {
+        if (powerProvider.useEnergy(Settings.mjPerNv, Settings.mjPerNv, true) < Settings.mjPerNv) return;
+        outputBuffer.fill(new LiquidStack(ModObjects.itemLiquidNavitas, Settings.mjPerNv), true);
+
+        // Repeat if possible (see BC machines)
+        if (powerProvider.getEnergyStored() >= Settings.mjPerNv) {
+            doWork();
+        }
+    }
+
+    @Override
+    public int powerRequest() {
+        if (canDoWork()) return Settings.mjPerNv;
+        return 0;
+    }
+
+    private boolean canDoWork() {
+        LiquidStack stored = outputBuffer.getLiquid();
+        if (stored == null) return true;
+        // 2 MJ/t block so check for space
+        return stored.amount <= outputBuffer.getCapacity() - (Settings.mjPerNv / 2);
+    }
 
 	@Override
 	public int fill(ForgeDirection from, LiquidStack resource, boolean doFill) {
@@ -86,7 +149,17 @@ public class TileLiquifierMJ extends TileEntity implements ITankContainer {
 	}
 
 	public void sendDebugToPlayer(EntityPlayer player) {
-		// TODO: Debug info
+        try {
+		    player.addChatMessage("[TileLiquifierMJ] powerProvider: " + powerProvider.toString());
+            LiquidStack l = outputBuffer.getLiquid();
+            if (l != null) {
+                player.addChatMessage("[TileLiquifierMJ] outputBuffer: " + l.amount + "/8000");
+            } else {
+                player.addChatMessage("[TileLiquifierMJ] outputBuffer: 0/8000");
+            }
+        } catch (Exception e) {
+            player.addChatMessage("[TileLiquifierMJ] Exception getting debug: " + e.getMessage());
+        }
 	}
 
 	// Read/Write NBT overrides for buffer
--- common/io/sunstrike/mods/liquidenergy/configuration/Settings.java
+++ common/io/sunstrike/mods/liquidenergy/configuration/Settings.java
@@ -4,15 +4,32 @@ import net.minecraftforge.common.Configuration;
 
 /**
  * Settings
+ * io.sunstrike.mods.liquidenergy.configuration
+ * LiquidEnergy
  *
- * This class should only hold user-adjustable settings.
- * Block and item instances belong in ModObjects!
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Sunstrike <sunstrike@azurenode.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @author Sunstrike <sunstrike@azurenode.net>
  */
 
 public class Settings {
-	
+
+	// To change defaults, change these variables before compile
+
 	// Blocks
 	public static int blockLiquifyEU = 1460;
 	public static int blockLiquifyMJ = 1461;
@@ -24,23 +41,23 @@ public class Settings {
 	
 	// Conversions
 	public static int euPerNv = 8;
-	public static int mjPerNv = 1;
+	public static int mjPerNv = 2;
 
     public static void doConfig(Configuration config) {
         config.load();
 
         // Blocks
-        blockLiquifyEU = config.getBlock("BlockLiquifyEU", 1460).getInt();
-        blockLiquifyMJ = config.getBlock("BlockLiquifyMJ", 1461).getInt();
-        blockGeneratorEU = config.getBlock("BlockGeneratorEU", 1462).getInt();
-        blockGeneratorMJ = config.getBlock("BlockGeneratorMJ", 1463).getInt();
+        blockLiquifyEU = config.getBlock("BlockLiquifyEU", blockLiquifyEU).getInt();
+        blockLiquifyMJ = config.getBlock("BlockLiquifyMJ", blockLiquifyMJ).getInt();
+        blockGeneratorEU = config.getBlock("BlockGeneratorEU", blockGeneratorEU).getInt();
+        blockGeneratorMJ = config.getBlock("BlockGeneratorMJ", blockGeneratorMJ).getInt();
 
         // Items
-        itemLiquidNavitas = config.getItem("LiquidNavitas", 1464).getInt();
+        itemLiquidNavitas = config.getItem("LiquidNavitas", itemLiquidNavitas).getInt();
 
         // Conversion values
-        euPerNv = config.get("conversions", "euPerNv", 8).getInt();
-        mjPerNv = config.get("conversions", "mjPerNv", 1).getInt();
+        euPerNv = config.get("conversions", "euPerNv", euPerNv).getInt();
+        mjPerNv = config.get("conversions", "mjPerNv", mjPerNv).getInt();
 
         config.save();
     }
