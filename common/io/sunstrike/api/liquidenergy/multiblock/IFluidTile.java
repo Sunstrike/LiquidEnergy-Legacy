21c9dd70f2bb9dd925c830aedb3c8b0f6ca307a8
Sunstrike <sunstrike@azurenode.net>
2013-02-27 09:36:08 +0000

    Multiblock interfaces
    Signed-off-by: Sunstrike <sunstrike@azurenode.net>

---
+++ common/io/sunstrike/api/liquidenergy/multiblock/IFluidTile.java
@@ -0,0 +1,70 @@
+package io.sunstrike.api.liquidenergy.multiblock;
+
+import net.minecraftforge.liquids.ITankContainer;
+import net.minecraftforge.liquids.LiquidStack;
+
+/*
+ * IFluidTile
+ * io.sunstrike.api.liquidenergy.multiblock
+ * LiquidEnergy
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Sunstrike <sunstrike@azurenode.net>
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * @author Sunstrike <sunstrike@azurenode.net>
+ */
+
+/**
+ * Interface to be implemented in all fluid blocks for IStructure structures.
+ * </p>
+ * Fluid blocks must also implement ITankContainer but redirect these calls to the IStructure object, else do nothing
+ * if no structure is attached (central tank handles fluids instead of the input tiles).
+ *
+ * @author Sunstrike <sunstrike@azurenode.net>
+ */
+public interface IFluidTile extends ITankContainer {
+
+    /**
+     * Setter method for the structure this block is attached to
+     *
+     * @param structure IStructure object to attach to
+     */
+    public void setStructure(IStructure structure);
+
+    /**
+     * Getter method for the structure this block is attached to
+     *
+     * @return IStructure this is attached to
+     */
+    public IStructure getStructure();
+
+    /**
+     * Tell the caller if this can be used as an output
+     *
+     * @return True if can act as an output, else false.
+     */
+    public boolean canDumpOut();
+
+    /**
+     * Attempt to dump liquids out of this output (nullable)
+     *
+     * @param resource LiquidStack to attempt to eject
+     * @param doFill Whether or not to commit the action
+     * @return Amount dumped to an ITankContainer
+     */
+    public int dump(LiquidStack resource, boolean doFill);
+
+}
